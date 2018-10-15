81451ef55881245e038e24a49b4f8ed4e3987517
ziyu_eric_xie <xieziyu18@hotmail.com>
2018-10-15 15:18:48 -0700

    FB authenticaltion

--- app/src/main/java/com/conpeto/nullpointer/conpeto/LoginActivity.java
+++ app/src/main/java/com/conpeto/nullpointer/conpeto/LoginActivity.java
@@ -1,31 +1,61 @@
 package com.conpeto.nullpointer.conpeto;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
+    CallbackManager callbackManager;
+    LoginButton loginButton;
+    TextView textView;
+    private static final String EMAIL = "email";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+        textView = (TextView) findViewById(R.id.text1);
+        // Callback registration
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onSuccess(LoginResult loginResult) {
+                textView.setText("Login Succeed \n" + loginResult.getAccessToken().getToken());
+                Log.d("token return is",loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                // need to add more if needed
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // need to add more if needed
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
