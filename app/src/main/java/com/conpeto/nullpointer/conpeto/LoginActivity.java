0d8c5bb0fd3cf04fe7985878300e2b86a992534e
ziyu_eric_xie <xieziyu18@hotmail.com>
2018-10-20 19:31:05 -0700

    done with Integration with addUser

--- app/src/main/java/com/conpeto/nullpointer/conpeto/LoginActivity.java
+++ app/src/main/java/com/conpeto/nullpointer/conpeto/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.conpeto.nullpointer.conpeto;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,13 +15,27 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Arrays;
+
+import org.apache.http.NameValuePair;
+
+import static com.facebook.HttpMethod.POST;
 
 public class LoginActivity extends AppCompatActivity {
     CallbackManager callbackManager;
     LoginButton loginButton;
     TextView textView;
     private static final String EMAIL = "email";
+    private String userID;
+    private String tempPW = "12345";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +45,13 @@ public class LoginActivity extends AppCompatActivity {
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions(Arrays.asList(EMAIL));
         textView = (TextView) findViewById(R.id.text1);
-        // Callback registration
-        // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Log.d("token return is",loginResult.getAccessToken().getToken());
-                // todo 1: right now just skip authentication, assume it is already there, will have to incorporate it later on
-
-
-                // todo 2 : jump to PostLogin page, and using the add_user API
-
+                userID = loginResult.getAccessToken().getUserId();
+                AddUser addUser = new AddUser();
+                addUser.execute();
             }
 
             @Override
@@ -53,7 +64,6 @@ public class LoginActivity extends AppCompatActivity {
                 // need to add more if needed
             }
         });
-
     }
 
     @Override
@@ -82,5 +92,77 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private class AddUser extends AsyncTask<Void, Integer, String> {
+        protected String doInBackground(Void...params) {
+            String urlString = "http://null-pointers.herokuapp.com/user";
+            StringBuffer response = new StringBuffer();
+            try {
+                Log.e("before connection","let's start");
+                URL url = new URL(urlString);
+                HttpURLConnection client = (HttpURLConnection) url.openConnection();
+                System.out.println("After connection\n");
+                client.setRequestProperty("Content-Type","application/json");
+                client.setRequestMethod("POST");
+                client.setDoOutput(true);
+
+                // append the content in JSON format
+                StringBuilder body = new StringBuilder();
+                body.append("{\"user\":");
+                body.append(userID);
+                body.append(", ");
+                body.append("\"pass\":");
+                body.append(tempPW);
+                body.append("}");
+
+                String userInfo = body.toString();
+                Log.d("Body is",userInfo);
+
+                // Send post request
+                DataOutputStream wr = new DataOutputStream(client.getOutputStream());
+                System.out.println("before Write\n");
+                wr.writeBytes(userInfo);
+                wr.flush();
+                wr.close();
+
+                int responseCode = client.getResponseCode();
+                System.out.println("\nSending 'POST' request to URL : " + url);
+                System.out.println("Post body : " + userInfo);
+                System.out.println("Response Code : " + responseCode);
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(client.getInputStream()));
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+            } catch(MalformedURLException E){Log.e( "URL","The URL is not correct");}
+            catch(IOException E){}
+
+            return response.toString();
+        }
+
+        protected void onProgressUpdate(Integer...parms) {
+            super.onProgressUpdate();
+        }
+
+        protected void onPostExecute(String result) {
+            Log.e("onPostExecute response:",result);
+            if(result.contains("Added")||result.contains("Exists")){
+                Intent myIntent = new Intent(LoginActivity.this, PostLogin.class);
+                 LoginActivity.this.startActivity(myIntent);
+            }
+            else
+                return;
+        }
+    }
+
+    public String getUserID() {
+        return userID;
     }
 }
