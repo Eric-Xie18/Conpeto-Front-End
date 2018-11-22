cbb4307ec3eda4e56885ea4c945930e9e01e9a1d
ziyu_eric_xie <xieziyu18@hotmail.com>
2018-11-21 21:25:19 -0800

    Finish refactoring API/ UI and logic with Create Group

--- app/src/main/java/com/conpeto/nullpointer/conpeto/CreateGroup.java
+++ app/src/main/java/com/conpeto/nullpointer/conpeto/CreateGroup.java
@@ -9,10 +9,14 @@ import android.support.v7.app.AppCompatActivity;
 //import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
@@ -31,13 +35,21 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class CreateGroup extends AppCompatActivity {
 
     private String userID;
     private ArrayList<String> groupInfo;
+    private String category = "Music and Arts";
+    private String location = null;
     int PLACE_PICKER_REQUEST = 1;
+    private Place place = null;
+    private double longitude = 0;
+    private double latitude = 0;
+    private TextView placeLocation;
 
      @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +59,45 @@ public class CreateGroup extends AppCompatActivity {
         final Button back = (Button) findViewById(R.id.button_Cancel);
         final Button submit = (Button) findViewById(R.id.button_Submit);
         final Button pickPlace = (Button)findViewById(R.id.pick_place);
+        placeLocation = (TextView) findViewById(R.id.textView3);
+         //get the spinner from the xml.
+         Spinner dropdown = findViewById(R.id.ddCategory);
+//create a list of items for the spinner.
+         String[] items = new String[]{ "Music and Arts", "Sport", "Food and Conversation"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+         dropdown.setAdapter(adapter);
+         dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+             @Override
+             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
+                 switch (i) {
+                     case 0:
+                         category = "Music and Arts";
+                         break;
+                     case 1:
+                         // Whatever you want to happen when the second item gets selected
+                         category = "Sport";
+                         break;
+                     case 2:
+                         // Whatever you want to happen when the thrid item gets selected
+                         category = "Food and Conversation";
+                         break;
+                 }
+             };
+
+             @Override
+             public void onNothingSelected(AdapterView<?> adapterView) {
+
+             };
+
+
+         });
+
+
+        System.out.println("In create group: The radius is" + PostLogin.gerRadius());
         pickPlace.setOnClickListener( new View.OnClickListener(){
                 public void onClick(View v) {
                     PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
@@ -71,29 +121,46 @@ public class CreateGroup extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 EditText group_Name = (EditText) findViewById(R.id.group_Name);
-                EditText group_Activity = (EditText) findViewById(R.id.group_Activity);
                 String name = group_Name.getText().toString();
-                String activity = group_Activity.getText().toString();
                 System.out.println("group name is" + name);
-                System.out.println("Activity name is" + activity);
 
-                if ("".equals(name) || "".equals(activity)) {
-                    Toast.makeText(CreateGroup.this, "You must specify either the group name or group activity before proceed",
+
+                Pattern p = Pattern.compile("[^a-z 0-9 ]", Pattern.CASE_INSENSITIVE);
+                Matcher m = p.matcher(group_Name.getText().toString());
+
+                // need a third case where non a~Z and 0~9 character appears
+                if ("".equals(name) || "".equals(group_Name.getText().toString())) {
+                    Toast.makeText(CreateGroup.this, "You must specify either the group name to proceed.",
                             Toast.LENGTH_LONG).show();
-                } else {
+                }
+                else if (m.find()){
+                    Toast.makeText(CreateGroup.this, "The group name can only contain letters a-z, number 0-9 and white spaces.",
+                            Toast.LENGTH_LONG).show();
+                }
+                else if(location==null){
+                    Toast.makeText(CreateGroup.this, "Please pick a location for your group activity",
+                            Toast.LENGTH_LONG).show();
+
+                }
+                else {
                     // user_id
                     // group_name
                     // group_Details
-                    // need date and time as well
-                    String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-                    String time = new SimpleDateFormat("HH:mm").format(new Date());
+                    // group_Category
+                    // “group_latitude”:”123”, “group_longitude”:”123”
+                   EditText groupDetails = (EditText)findViewById(R.id.group_details);
+                   String details = groupDetails.getText().toString();
+                   if(details.equals(null)||details.equals("")){
+                       details = " ";
+                   }
 
                     groupInfo = new ArrayList<String>();
                     groupInfo.add(userID);
                     groupInfo.add(name);
-                    groupInfo.add(activity);
-                    groupInfo.add(date);
-                    groupInfo.add(time);
+                    groupInfo.add(details);
+                    groupInfo.add(category);
+                    groupInfo.add(Double.toString(latitude));
+                    groupInfo.add(Double.toString(longitude));
                     NewGroup createGroup = new NewGroup();
                     createGroup.execute();
                 }
@@ -106,18 +173,35 @@ public class CreateGroup extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PLACE_PICKER_REQUEST) {
             if (resultCode == RESULT_OK) {
-                Place place = PlacePicker.getPlace(data, this);
-                String toastMsg = String.format("Place: %s", place.getName());
-                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+                place = PlacePicker.getPlace(data, this);
+                if (place == null) {
+                    String toastMsg = String.format("Can't get the place");
+                    Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+
+                } else {
+                    String toastMsg = String.format("Place: %s", place.getName());
+                    Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+                    longitude = place.getLatLng().longitude;
+                    latitude = place.getLatLng().latitude;
+                    location = place.getName().toString();
+                    placeLocation.setText(location);
+
+                }
             }
         }
     }
+    protected void onResume(){
+         super.onResume();
+         System.out.println("Resume Create group: The radius is" + PostLogin.gerRadius());
 
 
-    private class NewGroup extends AsyncTask<Void, Integer, String> {
-        protected String doInBackground(Void... params) {
+    }
+
+    private class NewGroup extends AsyncTask<Void, Integer, Integer> {
+        protected Integer doInBackground(Void... params) {
             String urlString = "http://null-pointers.herokuapp.com/group";
             StringBuffer response = new StringBuffer();
+            int responseCode = 400;
             try {
                 URL url = new URL(urlString);
                 HttpURLConnection client = (HttpURLConnection) url.openConnection();
@@ -127,8 +211,16 @@ public class CreateGroup extends AppCompatActivity {
                 client.setDoOutput(true);
 
                 // append the content in JSON format
+                // user_id
+                // group_name
+                // group_Details
+                // group_Category
+                // “group_latitude”:”123”, “group_longitude”:”123”
+                // {“id”:”12345”, “group_name”:”new_group”, “group_details”:
+                //“new_group_details”, “group_category”: “Sport”, “group_latitude”:”123”, “group_longitude”:”123”}
+
                 StringBuilder body = new StringBuilder();
-                body.append("{\"user\":");
+                body.append("{\"id\":");
                 body.append("\"");
                 body.append(groupInfo.get(0));
                 body.append("\"");
@@ -143,14 +235,19 @@ public class CreateGroup extends AppCompatActivity {
                 body.append(groupInfo.get(2));
                 body.append("\"");
                 body.append(", ");
-                body.append("\"date\":");
+                body.append("\"group_category\":");
                 body.append("\"");
                 body.append(groupInfo.get(3));
                 body.append("\"");
                 body.append(", ");
-                body.append("\"time\":");
+                body.append("\"group_latitude\":");
                 body.append("\"");
                 body.append(groupInfo.get(4));
+                body.append("\"");
+                body.append(", ");
+                body.append("\"group_longitude\":");
+                body.append("\"");
+                body.append(groupInfo.get(5));
                 body.append("\"");
 
                 body.append("}");
@@ -165,7 +262,7 @@ public class CreateGroup extends AppCompatActivity {
                 wr.flush();
                 wr.close();
 
-                int responseCode = client.getResponseCode();
+                responseCode = client.getResponseCode();
                 System.out.println("\nSending 'POST' request to URL : " + url);
                 System.out.println("Post body : " + userInfo);
                 System.out.println("Response Code : " + responseCode);
@@ -184,22 +281,34 @@ public class CreateGroup extends AppCompatActivity {
             } catch (IOException E) {
             }
 
-            return response.toString();
+            System.out.println("onPostExecute response: " + response.toString());
+            return responseCode;
+
         }
+
 
         protected void onProgressUpdate(Integer... parms) {
             super.onProgressUpdate();
         }
 
-        protected void onPostExecute(String result) {
+        protected void onPostExecute(Integer result) {
             // add toast message for added and not added
-            Log.e("onPostExecute response:", result);
+            if(result==200){
             Intent goBack = new Intent(CreateGroup.this, PostLogin.class);
             Toast.makeText(CreateGroup.this, "Group Created!",
                     Toast.LENGTH_LONG).show();
-            goBack.putExtra("user_ID", userID);
-            CreateGroup.this.startActivity(goBack);
         }
+          else if(result==409){
+                Toast.makeText(CreateGroup.this, "Group already exists, please use the other name",
+                        Toast.LENGTH_LONG).show();
+            }
+
+            else{
+                Toast.makeText(CreateGroup.this, "Create group failed",
+                        Toast.LENGTH_LONG).show();
+
+            }
 
     };
+}
 }
--- app/src/main/java/com/conpeto/nullpointer/conpeto/PostLogin.java
+++ app/src/main/java/com/conpeto/nullpointer/conpeto/PostLogin.java
@@ -10,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 /*import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -30,10 +31,12 @@ private static String radius = "5";
         final Button bCreateGroup = findViewById(R.id.create_Group);
         final Button bViewGroup = findViewById(R.id.view_groups);
         final Button bJoinGroup = findViewById(R.id.search_Group);
+        final TextView radiusDislay = findViewById(R.id.textView2);
+
         //get the spinner from the xml.
         Spinner dropdown = findViewById(R.id.spinner1);
 //create a list of items for the spinner.
-        String[] items = new String[]{"5 KM", "10 KM", "15 KM","30 KM","Any"};
+        String[] items = new String[]{"Last Saved Setting", "5 KM", "10 KM", "15 KM","30 KM","Any"};
 //create an adapter to describe how the items are displayed, adapters are used in several places in android.
 //There are multiple variations of this, but this is the basic variant.
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
@@ -46,25 +49,36 @@ private static String radius = "5";
                                                    switch (i) {
                                                        case 0:
                                                            // Whatever you want to happen when the first item gets selected
-                                                           radius = "5";
+                                                           radius = radius;
+                                                           radiusDislay.setText("The current filter distance is" + radius);
                                                            break;
                                                        case 1:
                                                            // Whatever you want to happen when the second item gets selected
-                                                           radius = "10";
+                                                           radius = "5";
+                                                           radiusDislay.setText("The current filter distance is" + radius);
                                                            break;
                                                        case 2:
                                                            // Whatever you want to happen when the thrid item gets selected
-                                                           radius = "15";
+                                                           radius = "10";
+                                                           radiusDislay.setText("The current filter distance is" + radius);
                                                            break;
 
                                                        case 3:
                                                            // Whatever you want to happen when the thrid item gets selected
-                                                           radius = "30";
+                                                           radius = "15";
+                                                           radiusDislay.setText("The current filter distance is" + radius);
                                                            break;
 
                                                        case 4:
                                                            // Whatever you want to happen when the thrid item gets selected
+                                                           radius = "30";
+                                                           radiusDislay.setText("The current filter distance is" + radius);
+                                                           break;
+
+                                                           case 5:
+                                                           // Whatever you want to happen when the thrid item gets selected
                                                            radius = "Any";
+                                                               radiusDislay.setText("The geo filter distance is turned off");
                                                            break;
 
                                                    }
@@ -72,7 +86,7 @@ private static String radius = "5";
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-
+                          radius = radius;
             };
 
 
@@ -103,8 +117,12 @@ private static String radius = "5";
         });
 
     }
+      @Override
+     protected void onResume(){
+        super.onResume();
+        System.out.println("In post login on Resume The radius is" + radius);
 
-
+     }
 
     public static String gerRadius(){
         return radius;
